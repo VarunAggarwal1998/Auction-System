21cccdf21d4f86ae39a5cf32aa07201d525137a8
VarunAggarwal1998 <117046986+VarunAggarwal1998@users.noreply.github.com>
2023-11-06 01:46:18 -0500

    changes

--- commerce/settings.py
+++ commerce/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'commerce.wsgi.application'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
