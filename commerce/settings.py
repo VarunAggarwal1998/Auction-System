de58327e6799661e714b5b2d989fb7a8444d0878
VarunAggarwal1998 <117046986+VarunAggarwal1998@users.noreply.github.com>
2023-11-24 02:21:21 -0500

    update procfile

--- commerce/settings.py
+++ commerce/settings.py
@@ -35,7 +35,8 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['auction-arena-varun-88fe80c71a78.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['auction-cloud-varun-a02a5113a697.herokuapp.com', 'localhost', '127.0.0.1']
+
 
 
 
